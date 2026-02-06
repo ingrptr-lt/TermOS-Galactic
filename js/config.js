97573ed86946056056a323773a384ec90f81d643
ingrptr-lt <ingrptr@gmail.com>
2026-02-06 18:26:00 +0200

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,8 @@
+export const CONFIG = {
+  MQTT_BROKER: "broker.emqx.io",
+  MQTT_PORT: 8084,
+  MQTT_PATH: "/mqtt",
+  DEFAULT_MODEL: "llama-3.3-70b-versatile",
+  STORAGE_KEY: "termos_groq_key",
+  LANG_KEY: "termos_lang"
+};
